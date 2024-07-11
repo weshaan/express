d46bda5a3585855e8be3ea051f09648042265b05
Eshaan Walia <118920744+weshaan@users.noreply.github.com>
2024-07-11 16:40:01 +0530

    router for books

---
+++ src/routes/books.js
@@ -0,0 +1,46 @@
+const {Router} = require('express');
+
+const router = Router();
+
+const books = [
+    {
+      id:1,
+      item: 'Bhagavad Gita',
+      quantity: 2000
+    },
+    {
+      id:2,
+      item: 'Srimad Bhagavatam',
+      quantity: 100
+    },
+    {
+      id:3,
+      item: 'Science of self realisation',
+      quantity: 500
+    },
+    {
+      id:4,
+      item: 'Prabhupada Lilamrita',
+      quantity: 1000
+    }
+  ]
+
+router.get('/', (request, response) => {
+    console.log(request.query)
+    response.send(books)
+})
+
+router.get('/:item',(request,response) => {
+  const item = request.params.item
+  const book = books.find((book) => book.item === item)
+  response.send(book)
+  })
+
+router.post('/', (request, response) => {
+    console.log("request.body")
+    console.log(request.body)
+    books.push(request.body)
+    response.send(201)
+})
+
+module.exports = router;
