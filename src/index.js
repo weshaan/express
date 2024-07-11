e68c837f90a77e700de5c7e07ffc1dc9614c76ee
Eshaan Walia <118920744+weshaan@users.noreply.github.com>
2024-07-11 16:39:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,42 +1,10 @@
 const express = require('express')
+const booksrouter = require('./routes/books')
 
 const app = express()
 const PORT = 3000
 app.use(express.json());
-const books = [
-    {
-      id:1,
-      item: 'Bhagavad Gita',
-      quantity: 2000
-    },
-    {
-      id:2,
-      item: 'Srimad Bhagavatam',
-      quantity: 100
-    },
-    {
-      id:3,
-      item: 'Science of self realisation',
-      quantity: 500
-    },
-    {
-      id:4,
-      item: 'Prabhupada Lilamrita',
-      quantity: 1000
-    }
-  ]
-
-  // 1. Get Req: Define your path as specific as possible
-app.get('/', (request, response) => {
-    console.log(request.query)
-    response.send(books)
-})
-
-app.post('/', (request, response) => {
-    console.log("request.body")
-    console.log(request.body)
-    books.push(request.body)
-    response.send(201)
-})
+app.use(express.urlencoded());
+app.use('/api',booksrouter)
 
 app.listen(PORT, () => console.log(`Running express server on port ${PORT}`))
