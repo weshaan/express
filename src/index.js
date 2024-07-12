52bafebcd36df5842e851a392b25dac68ed49688
Eshaan Walia <118920744+weshaan@users.noreply.github.com>
2024-07-12 13:30:51 +0530

    add sessions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const booksrouter = require('./routes/books')
+const session = require('express-session')
 
 const app = express()
 const PORT = 3000
 app.use(express.json());
 app.use(express.urlencoded());
-app.use('/api',booksrouter)
+app.use('/api/v1',booksrouter)
+app.use(session({
+    secret: 'uwejuewimwniuenownfwonfownwo',
+    resave: false,
+    saveUninitialized: true,
+    }));
 
 app.listen(PORT, () => console.log(`Running express server on port ${PORT}`))
