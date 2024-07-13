abf3b2254d9cae3f7bdccdc9693452cb87dafab1
Eshaan Walia <118920744+weshaan@users.noreply.github.com>
2024-07-13 11:50:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-const express = require('express')
-const booksrouter = require('./routes/books')
-const session = require('express-session')
+const express = require('express');
+const booksrouter = require('./routes/books');
+const session = require('express-session');
+const authrouter = require('./routes/auth');
+require('./database');
 
 const app = express()
 const PORT = 3000
+
 app.use(express.json());
 app.use(express.urlencoded());
-app.use('/api/v1',booksrouter)
 app.use(session({
     secret: 'uwejuewimwniuenownfwonfownwo',
     resave: false,
     saveUninitialized: true,
     }));
+
+
+app.use('/api/v1',booksrouter)
+app.use('/api/v1',authrouter)
 
 app.listen(PORT, () => console.log(`Running express server on port ${PORT}`))
